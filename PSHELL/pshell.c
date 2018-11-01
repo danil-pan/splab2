5e171d014ebf1ca7696beb80da77f5a4f1381d55
danil-pan <43602142+danil-pan@users.noreply.github.com>
2018-11-01 12:49:31 +0600

    Update pshell.c

--- PSHELL/pshell.c
+++ PSHELL/pshell.c
@@ -1,1 +1,36 @@
-// your code here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define max_length 100
+int main(int argc, char *argv[]){
+	int p[100];
+	int n=0; 
+	for ( ;; ) {
+		printf("Give a command: ");
+		char c[max_length];
+		if (!fgets(c,100,stdin)){
+			printf("good bye\n");
+			break;
+		}
+		if (strcmp(c,"showjobs\n")==0){
+			int i;
+			for(i=0;i<n;i++){
+				printf("[%d]\n",p[i]);
+			}
+		} else {
+			int x = fork();
+			if (x==0){
+			  c[strlen(c)-1]=0; ///////
+			  execlp(c,c,NULL);
+			  exit(0);	    ///////
+			}
+			
+			//waitpid(x,0,0); 
+			p[n++]=x;
+		}
+	}
+		
+	return 0;
+}
